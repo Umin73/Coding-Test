8be88252884029c18716a25f7aa0244d0b040c93
yumin <trillionumin@gmail.com>
2024-08-05 20:14:27 +0900

    240805

---
+++ src/main/java/_4_정렬/H_Index.java
@@ -0,0 +1,34 @@
+package _4_정렬;
+
+import java.util.Arrays;
+
+public class H_Index {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{3,0,6,1,5}));             // 3
+        System.out.println(solution(new int[]{0}));                     // 0
+        System.out.println(solution(new int[]{3,4}));                   // 2
+        System.out.println(solution(new int[]{1,2,3,5,6,7,10,11}));     // 5
+        System.out.println(solution(new int[]{3,5,11,6,1,5,3,3,1,41})); // 5
+        System.out.println(solution(new int[]{1,11,111,1111}));         // 3
+        System.out.println(solution(new int[]{3,3,3,4}));               // 3
+    }
+
+    public static int solution(int[] citations) {
+        int h_index = 0;
+
+        Arrays.sort(citations);
+
+        for (int j = 1; j <= citations.length; j++) {
+            int count = 0;
+            int h = j;
+            for (int i = 0; i < citations.length; i++) {
+                if (citations[citations.length - 1 - i] >= h) count++;
+            }
+            if (h <= count) {
+                h_index = h;
+            }
+        }
+
+        return h_index;
+    }
+}
---
+++ src/main/java/_4_정렬/가장큰수.java
@@ -0,0 +1,29 @@
+package _4_정렬;
+
+import java.util.Arrays;
+
+public class 가장큰수 {
+    public static void main(String[] args) {
+
+        System.out.println(solution(new int[]{6,10,2}));                // answer : 6210
+        System.out.println(solution(new int[]{3,30,34,5,9}));           // answer : 9534330
+        System.out.println(solution(new int[]{979,97,978,81,818,817})); // answer : 9799797881881817
+        System.out.println(solution(new int[]{0,0,0,0}));
+    }
+
+    public static String solution(int[] numbers) {
+        StringBuilder answer = new StringBuilder();
+
+        String[] numbersStr = new String[numbers.length];
+        for (int i=0; i< numbers.length; i++) {
+            numbersStr[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(numbersStr, ((o1, o2) -> (o2+o1).compareTo(o1+o2)));
+
+        for(String s : numbersStr) {
+            answer.append(s);
+        }
+
+        return answer.toString().charAt(0) == '0' ? "0" : answer.toString();
+    }
+}
