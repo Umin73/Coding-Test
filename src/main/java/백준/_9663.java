327b60230f8da90d7a45a46e6661b48ddae3960c
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-03-21 12:57:39 +0900

    250321

--- src/main/java/백준/_9663.java
+++ src/main/java/백준/_9663.java
@@ -6,46 +6,37 @@ import java.io.InputStreamReader;
 
 public class _9663 {
     static int N;
-    static int answer = 0;
     static int[] arr;
-    static int solution(int depth) {
-        // depth라고 썼지만, 간단하게 depth번째 퀸이라 생각하면 됨
+    static int answer = 0;
+
+    static void solution(int depth) {
         if(depth == N) {
             answer++;
-            return depth;
+            return;
         }
 
-        for(int i=0; i < N; i++) {
-            arr[depth] = i; // depth번째 퀸을 i열에 위치시킴
-            if(possible(depth)) { // i열에 위치시킨 depth번째 퀸이 이 위치에 놓아도 되는지
-                solution(depth+1); // 다음 순서의 퀸의 위치를 정하러 감
-            }
+        for(int i=0; i<N; i++) {
+            arr[depth] = i;
+            if(isPossible(depth)) solution(depth+1);
         }
-        return depth;
     }
 
-    static boolean possible(int col) {
-        for(int i=0; i< col; i++) { // 이전에 놓였던 퀸들을 확인
-
-            // 같은 열에 퀸이 있는 경우
+    static boolean isPossible(int col) {
+        for(int i=0; i<col; i++) {
             if(arr[i] == arr[col]) return false;
-            // 대각선에 퀸이 있는 경우
-            else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])) return false;
+            if(Math.abs(col - i) == Math.abs(arr[col]-arr[i])) return false;
         }
-        return true; // 현재 퀸이 다른 퀸들과 서로 공격할 수 없는 위치에 있으면 true
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-
         arr = new int[N];
         /*
-        * N*N 체스판이면 2차원 배열 써야 할 것 같은데 왜 1차원 배열을 썼나?
-        * -> 한 행 or 한 열에 하나의 퀸밖에 오지 못함
-        *    같은 줄에 있으면 공격 가능해짐
+        * 퀸은 가로, 세로, 대각선으로 움직일 수 있음
+        * -> 현재 퀸의 위치에서 같은 행, 열, 대각선에 다른 퀸 존재할 수 X
         * */
-
         solution(0);
         System.out.println(answer);
         br.close();
