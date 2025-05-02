693df2dd593779190261a1a40c230192f2bb2b73
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-05-02 21:47:54 +0900

    ~250502

---
+++ src/main/java/백준/_1629.java
@@ -0,0 +1,41 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class _1629 {
+
+    static long A, B, C;
+
+    static long myPow(long a, long b) {
+        long answer = 1;
+
+//        10^11 == (10^5)^2*a
+//        10^10 == (10^5)^2
+        while(b>0) {
+            if(b%2==1) {
+                answer = answer * a % C;
+            }
+            a = (a*a)%C;
+            b /= 2;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        A = Long.parseLong(input[0]);
+        B = Long.parseLong(input[1]);
+        C = Long.parseLong(input[2]);
+
+        if(A == 0) System.out.println(0);
+        else if(B == 0) System.out.println(A%C);
+        else System.out.println(myPow(A,B));
+
+        br.close();
+    }
+}
