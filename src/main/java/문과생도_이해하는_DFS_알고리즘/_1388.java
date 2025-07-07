65a2f86aafcdd1ef4d019dae37c06eecb98121bb
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-07-07 14:08:15 +0900

    250707

---
+++ src/main/java/문과생도_이해하는_DFS_알고리즘/_1388.java
@@ -0,0 +1,63 @@
+package 문과생도_이해하는_DFS_알고리즘;
+
+import java.io.*;
+public class _1388 {
+
+    static int N, M; // N: 세로, M: 가로
+    static char[][] arr;
+    static int[] dirs = {-1, 1};
+
+    static void dfsA(int curY, int curX) {
+        if(arr[curY][curX] != '-') return;
+
+        arr[curY][curX] = ' ';
+        for(int i=0; i<2; i++) {
+            int nextX = curX + dirs[i];
+            if(nextX >= 0 && nextX < M) dfsA(curY, nextX);
+        }
+    }
+
+    static void dfsB(int curY, int curX) {
+        if(arr[curY][curX] != '|') return;
+
+        arr[curY][curX] = ' ';
+        for(int i=0; i<2; i++) {
+            int nextY = curY + dirs[i];
+            if(nextY >= 0 && nextY < N) dfsB(nextY, curX);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        arr = new char[N][M];
+        for(int i=0; i<N; i++) {
+            input = br.readLine().split("");
+            for(int j=0; j<M; j++)  {
+                arr[i][j] = input[j].charAt(0);
+            }
+        }
+
+        int answer = 0;
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(arr[i][j] == '-') {
+                    dfsA(i, j);
+                    answer++;
+                }
+                else if(arr[i][j] == '|') {
+                    dfsB(i, j);
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+        br.close();
+    }
+}
---
+++ src/main/java/문과생도_이해하는_DFS_알고리즘/_16173.java
@@ -0,0 +1,49 @@
+package 문과생도_이해하는_DFS_알고리즘;
+
+import java.io.*;
+
+public class _16173 {
+
+    static int N;
+    static int[][] arr;
+    static boolean isSuccess = false;
+
+    static void dfs(int curY, int curX) {
+        if(arr[curY][curX] == -2) return;
+        if(arr[curY][curX] == -1) {
+            isSuccess = true;
+            return;
+        }
+
+        int jump = arr[curY][curX];
+        int[][] dirs = {{1, 0}, {0,1}};
+
+        arr[curY][curX] = -2;
+
+        for (int i=0; i<2; i++) {
+            int nextY = curY + (dirs[i][0] * jump);
+            int nextX = curX + (dirs[i][1] * jump);
+            if(nextY >=0 && nextY < N && nextX >=0 && nextX < N) {
+                dfs(nextY, nextX);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for(int i = 0; i < N; i++){
+            String[] input = br.readLine().split(" ");
+            for(int j = 0; j < N; j++){
+                arr[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+        dfs(0, 0);
+        System.out.println(isSuccess ? "HaruHaru" : "Hing");
+
+        br.close();
+    }
+}
